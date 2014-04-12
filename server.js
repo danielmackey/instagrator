62ff34d55a9ee309523081640f15316e2ca64591
Daniel Mackey <daniel@danielmackey.com>
2014-04-12 15:33:37 -0700

    Callback challenge beginning

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ var app = express();
 
 app.get('/', function(req, res) {
   res.send('#GRIDLIFE instagrator');
-})
+});
 
-app.listen(process.env.PORT || 5000);
+app.get('/instagram/', function(req, res) {
+  if(req.query['hub.verify_token'] === 'myVerifyToken') {
+    res.send(200, req.query['hub.challenge']);
+  } else {
+    res.send(401, null);
+  }
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function(){
+  console.log("ready captain on port " + port);
+});
